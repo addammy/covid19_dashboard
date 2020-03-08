5df49cf41e96531b5de5824cc2f25c3442086f7c
Anna Meringer <ania.meringer@gmail.com>
2020-03-08 19:06:02 +0100

    fix sorting problem

--- src/corona/comparisons.py
+++ src/corona/comparisons.py
@@ -26,7 +26,7 @@ def epidemic_summaries(cases_df, epidemics_sheet):
     df['R0min'] = '1,4'
     df['R0max'] = '3,9'
     others = pd.DataFrame(epidemics_sheet.worksheet('base').get_all_records())
-    df = others.append(df, ignore_index=True)
+    df = others.append(df, ignore_index=True, sort=True)
     for col in ['CFR', 'R0', 'R0min', 'R0max']:
         df[col] = df[col].str.replace(',', '.').astype(float)
     return df
@@ -40,5 +40,5 @@ def sars_progress(cases_df):
     :return: both_df, sars_df: (DataFrame, DataFrame) tuple. both_df contains concatenated data from cases_df and sars_df.
     """
     sars_df = pd.read_csv(resources.open_text('corona.resources', 'SARS.csv'))
-    both_df = pd.concat([cases_df, sars_df], ignore_index=True)
+    both_df = pd.concat([cases_df, sars_df], ignore_index=True, sort=True)
     return both_df, sars_df
